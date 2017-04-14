b0fbbe1023997e8f41848052686a9c562958f55e
mic <michael.aring@live.com>
2017-04-14 22:34:34 +0200

    base stuff

---
+++ de.tcl.sw.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace de.tcl.sw.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new de.tcl.sw.App());
+        }
+    }
+}
---
+++ de.tcl.sw/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using de.tcl.sw.ViewModels;
+
+using Xamarin.Forms;
+
+namespace de.tcl.sw.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
